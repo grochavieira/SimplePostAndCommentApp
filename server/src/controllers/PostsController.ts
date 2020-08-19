d57c06b7985953a811a358f197aa1444d677be43
Guilherme Rocha <guirocha.hopeisaba@gmail.com>
2020-08-18 22:08:11 -0300

    server can now accept image files

--- server/src/controllers/PostsController.ts
+++ server/src/controllers/PostsController.ts
@@ -5,7 +5,16 @@ class PostsController {
   async index(request: Request, response: Response) {
     const posts = await knex("posts").select("*");
 
-    return response.json(posts);
+    const serializedPosts = posts.map((post) => {
+      return {
+        ...post,
+        image_url: post.image
+          ? `http://localhost:3333/uploads/${post.image}`
+          : post.image,
+      };
+    });
+
+    return response.json(serializedPosts);
   }
   async show(request: Request, response: Response) {
     const { id } = request.params;
@@ -16,16 +25,36 @@ class PostsController {
       return response.status(404).json({ message: "Post not found." });
     }
 
-    return response.json(post);
+    const comments = await knex("posts")
+      .join("comments", "posts.id", "=", "comments.post_id")
+      .where("comments.post_id", id)
+      .select("comments.*");
+
+    const serializedPost = {
+      ...post,
+      image_url: post.image
+        ? `http://localhost:3333/uploads/${post.image}`
+        : post.image,
+    };
+
+    return response.json({ serializedPost, comments });
   }
   async create(request: Request, response: Response) {
     const { username, content } = request.body;
 
-    const post = {
-      image: "image",
-      username,
-      content,
-    };
+    let post;
+    if (request.file) {
+      post = {
+        image: request.file.filename,
+        username,
+        content,
+      };
+    } else {
+      post = {
+        username,
+        content,
+      };
+    }
 
     const ids = await knex("posts").insert(post);
     const post_id = ids[0];
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
-
+import multer from "multer";
+import multerConfig from "./config/multer";
 import PostsController from "./controllers/PostsController";
 import CommentsController from "./controllers/CommentsController";
 
@@ -7,9 +8,11 @@ const routes = express.Router();
 const postsController = new PostsController();
 const commentsController = new CommentsController();
 
+const upload = multer(multerConfig);
+
 routes.get("/posts", postsController.index);
 routes.get("/posts/:id", postsController.show);
-routes.post("/posts", postsController.create);
+routes.post("/posts", upload.single("image"), postsController.create);
 
 routes.get("/posts/:id/comments", commentsController.index);
 routes.post("/posts/:id/comments", commentsController.create);
